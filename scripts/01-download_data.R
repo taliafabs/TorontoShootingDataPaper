8003ded711bd110735ba9b03d9f4153b21c0e87f
Talia Fabregas <talia.fabregas@mail.utoronto.ca>
2024-01-17 00:50:50 +0000

    some small updates

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Talia Fabregas
+# Date: January 16 2024
+# Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -15,7 +15,7 @@ library(dplyr)
 library(janitor)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+the_raw_data <- NA
 
 
 
