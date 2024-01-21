8a81226f2bb55b216ad4f144aeaf7d2f375dece4
Talia Fabregas <talia.fabregas@mail.utoronto.ca>
2024-01-21 21:59:20 +0000

    data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -4,7 +4,7 @@
 # Date: January 16 2024
 # Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: Run 01-download_data first
+# Pre-requisites: Run 01-download_data first.
 
 
 #### Workspace setup ####
@@ -21,32 +21,5 @@ cleaned_data <-
   filter(count >= 1) |>
   tidyr::drop_na()
 
-# cleaned_data <-
-#   raw_data |>
-#   janitor::clean_names() |>
-#   select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-#   filter(wing_width_mm != "caw") |>
-#   mutate(
-#     flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-#                                    "1.35",
-#                                    flying_time_sec_first_timer)
-#   ) |>
-#   mutate(wing_width_mm = if_else(wing_width_mm == "490",
-#                                  "49",
-#                                  wing_width_mm)) |>
-#   mutate(wing_width_mm = if_else(wing_width_mm == "6",
-#                                  "60",
-#                                  wing_width_mm)) |>
-#   mutate(
-#     wing_width_mm = as.numeric(wing_width_mm),
-#     wing_length_mm = as.numeric(wing_length_mm),
-#     flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-#   ) |>
-#   rename(flying_time = flying_time_sec_first_timer,
-#          width = wing_width_mm,
-#          length = wing_length_mm
-#          ) |> 
-#   tidyr::drop_na()
-
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/shooting_analysis_data.csv")
+write_csv(cleaned_data, "outputs/data/cleaned_data.csv")
