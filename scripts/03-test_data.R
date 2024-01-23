11d487b08be0f64dfb067e5b8d2ec5b749af1df8
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-01-23 20:13:24 +0000

    wrote tests

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Test the data
 # Author: Talia Fabregas
-# Date: 18 January 2024
+# Date: 22 January 2024
 # Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Run 01-download_data and 02-data_cleaning first
@@ -12,3 +12,5 @@ library(dplyr)
 library(janitor)
 
 #### Test data ####
+
+# test that only data from 2014 to 2019, inclusive, is included
